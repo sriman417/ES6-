70da23c0bdb46085c45033c115fb9e0a5796ddad
Nainala Srimannarayana <105102064+sriman417@users.noreply.github.com>
2024-06-18 10:12:54 +0530

    Create padstart.js

---
+++ padstart.js
@@ -0,0 +1,7 @@
+let example = 'YouTube.com/CodingTutorials360';
+
+// console.log(example.padStart(100));
+// console.log(example.padEnd(1));
+
+console.log(example.padStart(100).length);
+console.log(example.padEnd(1));
