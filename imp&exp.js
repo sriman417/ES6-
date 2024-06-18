17a536884ea59b7b7b2dabb7ff2acbc4599f12bc
Nainala Srimannarayana <105102064+sriman417@users.noreply.github.com>
2024-06-18 10:10:05 +0530

    Create imp&exp.js

---
+++ imp&exp.js
@@ -0,0 +1,21 @@
+**Challenge**
+
+Inside of the file data.js, create a function add, that will receive 2 numbers and return the sum of them.
+Make sure to export this function.
+
+- Import the function add, into the index.js file
+- Create a variable result, that will hold the result of the function add when you call it and pass 2 numbers into it.
+- print into the console the value of the variable result;
+
+// my answer 
+
+export const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+
+import { add } from './data.js';         
+
+let result = add(3, 2);
+
+console.log(result);
