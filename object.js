cad7a697b830ce891cd95a9c6ce6ba10e1eed7f3
Nainala Srimannarayana <105102064+sriman417@users.noreply.github.com>
2024-06-18 09:49:43 +0530

    Create object.js

---
+++ object.js
@@ -0,0 +1,22 @@
+/*
+    **** Challenge ****
+    
+    For this challenge destruture the following object.
+*/
+
+const student = {
+    name: "Kyle",
+    age: 24,
+    projects: {
+        diceGame: "Two player dice game using JavaScript"
+    }
+}
+
+// my answer
+
+const { name, age, projects: { diceGame } } = student;
+
+console.log(name);
+console.log(age);
+
+console.log(diceGame);
